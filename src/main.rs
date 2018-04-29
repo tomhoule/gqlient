160d33dbb4f70dd59bdb68802ab37e3495a4531a
Tom Houlé <tom@tomhoule.com>
2018-04-29 20:31:33 +0200

    Sketch out query API

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 #[macro_use]
+extern crate failure;
+extern crate futures;
+#[macro_use]
 extern crate structopt;
 extern crate graphql_parser;
 #[macro_use]
 extern crate quote;
+extern crate serde;
 
 use std::path::{Path, PathBuf};
+use futures::prelude::*;
 
 #[derive(StructOpt)]
 struct Command {
@@ -15,6 +20,36 @@ struct Command {
 
 fn gather_queries(schemas: Vec<(PathBuf, String)>, dir: &Path) {
     let root = ::std::env::current_dir().expect("current directory can be reached");
+}
+
+struct Query<Variables, Response> {
+    response: ::std::marker::PhantomData<Response>,
+    variables: Variables,
+}
+
+#[derive(Debug, Fail)]
+enum QlientError {
+    #[fail(display = "http error")]
+    Http,
+    #[fail(display = "serialization error")]
+    Serialization,
+}
+
+impl<'response, Variables, Response> Query<Variables, Response>
+where
+    Response: serde::Deserialize<'response>,
+    Variables: serde::Serialize
+{
+    fn new(variables: Variables) -> Self {
+        Query {
+            response: ::std::marker::PhantomData,
+            variables,
+        }
+    }
+
+    fn execute(self, backend_url: &str) -> impl Future<Item = Response, Error = QlientError> {
+        unimplemented!()
+    }
 }
 
 fn main() {
